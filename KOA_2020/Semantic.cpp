ece79e0c555c67dde037767d159a5eb15cca5170
Harwex213 <harwex213@mail.ru>
2020-12-18 23:12:00 +0300

    Start Semantic

--- KOA_2020/Semantic.cpp
+++ KOA_2020/Semantic.cpp
@@ -2,6 +2,29 @@
 
 namespace Semantic
 {
+	void Start(const MFST::Mfst& mfst, const LT::LexTable& lextable, const IT::IdTable& idTable)
+	{
+		for (auto i = mfst.storestate.begin(); i != mfst.storestate.end(); i++)
+		{
+			// Если нашли правило с Инструкцией.
+			if (i->nrule == I_RULE)
+			{
+				// Смотрим по подходящей цепочке.
+				switch (i->nrulechain)
+				{
+				case Irule_IF_ELSE:
+				case Irule_IF:
+					
+					break;
+				case Irule_INIT:
+				case Irule_DECL_AND_INIT:
+
+					break;
+				}
+			}
+		}
+	}
+
 	bool CheckFunctionCountParams(int deFactoParamsCount, LT::Entry& entry, const IT::IdTable& idTable)
 	{
 		IT::Entry tempIdEntry = IT::GetEntry(idTable, entry.idxTI);
--- KOA_2020/Semantic.h
+++ KOA_2020/Semantic.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "LT.h"
+#include "MFST.h"
 
 namespace Semantic
 {
+	void Start(const MFST::Mfst& mfst, const LT::LexTable& lextable, const IT::IdTable& idTable);
 	bool CheckFunctionCountParams(int deFactoParamsCount, LT::Entry& entry, const IT::IdTable& idTable);
 }
